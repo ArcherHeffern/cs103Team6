a4a4920813f048dec0eb85041470c4b005b1b0af
ArcherHeffern <heffernarcher@gmail.com>
2023-03-13 03:13:57 -0400

    overhauled form page to not reload on request

--- ca01/server.py
+++ ca01/server.py
@@ -1,6 +1,9 @@
 from flask import request,redirect,url_for,Flask, render_template, abort
 from gpt import GPT
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app = Flask(__name__)
 gptAPI = GPT(os.environ.get('API_KEY'))
@@ -10,48 +13,53 @@ app.secret_key = b'_5#y2L"F4Q789789uioujkkljkl...8z\n\xec]/'
 
 @app.route('/')
 def index():
-    ''' display a link to the general query page '''
+    ''' display a link to all querys '''
     return render_template("index.html")
 
-@app.route('/api/archer', methods=["GET", "POST"])
+@app.route('/about')
+def about():
+    """An about page describing the project"""
+    return render_template('about.html')
+
+@app.route('/team')
+def team():
+    """a "team" page which has a short bio of each member of the team and what their role was"""
+    return render_template('team.html')
+
+"""Form page for each team member which ask the user for some input, then calls the appropriate GPT method to get the response, which it sends back to the browser."""
+
+@app.route('/archer', methods=["GET", "POST"])
 def archer():
     if request.method == 'GET':
-        return render_template("form.html", name="Archer", prompt="Write a story about a superhero whose power is to turn into a __ whenever they sneeze.", route="/api/archer")
+        return render_template("form.html", name="Archer", prompt="Write a story about a superhero whose power is to turn into a __ whenever they sneeze.", route="/archer")
     elif request.method == "POST":
         return gptAPI.archers_prompt(request.form['prompt'])
     else:
         abort(405)
-# TODO:  
-# a) an about page which explains what your program does
-
-@app.route('/about')
-def about():
 
 
-
-# b) a "team" page which has a short bio of each member of the team and what their role was
-@app.route('/team')
-def team():
-    
-
-# c) an index page with links to each of the team-members pages
-@app.route('/index')
-def index():
-    
-
-# d) a form page for each team member which ask the user for some input, then calls the appropriate GPT method to get the response, which it sends back to the browser.
-
-@app.route('/api/efren', methods=['GET', 'POST'])
+@app.route('/efren', methods=['GET', 'POST'])
 def efren():
-    if method == 'GET':
+    if request.method == 'GET':
         return render_template("index.html", )
-    elif method == 'POST':
-        prompt = request.form[inp]
+    elif request.method == 'POST':
+        prompt = request.form['prompt']
         gptAPI.efren_prompt(prompt)
-        return render_template("index.html",)
+        return render_template("form.html", name='Efren', prompt=prompt, route='/efren')
     else:
-        return 'unknown HTTP method: '+str(request.method)
+        abort(405)
 
+# @app.route('paras', methods=["GET", "POST"])
+# def paras():
+#     pass
+
+# @app.route('samir', methods=["GET", "POST"])
+# def paras():
+#     pass
+
+# @app.route('kelden', methods=["GET", "POST"])
+# def paras():
+#     pass
 
 if __name__=='__main__':
     # run the code on port 5001, MacOS uses port 5000 for its own service :(
