e4c6442da0178e08c80485d3af28890503ac5084
Archer <heffernarcher@gmail.com>
2023-03-24 15:07:32 -0400

    fixed spacing

--- PA03/transactions.py
+++ PA03/transactions.py
@@ -17,33 +17,41 @@ class Transaction():
             name varchar(20) UNIQUE NOT NULL
             );""", ())
 
+
     def get_categories(self):
         """Gets all catagories"""
         return self.run_query("SELECT rowid, * FROM category;", ())
+
 
     def create_category(self, catagory: str):
         """Creates a catagory"""
         self.run_query("INSERT INTO category values (?)", (catagory))
 
+
     def update_category(self, catagory_id: str):
         """Updates a catagory"""
         self.run_query("UPDATE catagory SET name=(?)", (catagory_id))
+
 
     def get_transaction(self):
         """Gets all transactions"""
         return self.run_query("SELECT * FROM transactions;", ())
 
+
     def get_transactions_by_year(self):
         """Gets all transactions ordered by year descending"""
         return self.run_query("SELECT * FROM transactions ORDER BY YEAR, MONTH, DAY DEC;", ())
+
 
     def get_transactions_by_month(self):
         """Gets transactions by month descending"""
         return self.run_query("SELECT * FROM transactions ORDER BY MONTH, DAY DEC;", ())
 
+
     def get_transaction_by_day(self):
         """Gets transactions ordered by day descending"""
         return self.run_query("SELECT * FROM transactions ORDER BY DAY DEC;", ())
+
 
     def create_transaction(self, transaction: dict):
         """Creates new Transaction: Takes dict with all transaction values as input"""
@@ -53,6 +61,7 @@ class Transaction():
                 ?, ?, ?, ?, ?, ?
                 )
                 """, (v for v in transaction))
+
 
     def delete_transaction(self, transaction_id):
         """Deletes transaction by transaction_id"""
