8a9e938bd332b9b97268c5700c42b7d8609fd953
EfrenHaskell <106392411+EfrenHaskell@users.noreply.github.com>
2023-03-25 22:05:35 -0400

    Update tracker.py
    Create task menu

--- PA03/tracker.py
+++ PA03/tracker.py
@@ -2,21 +2,36 @@
 Main method
 """
 
-def getJobRequest():
-    jobs = ["0. quit",
-            "1. show categories",
-            "2. add category",
-            "3. modify category",
-            "4. show transactions",
-            "5. add transaction",
-            "6. delete transaction",
-            "7. summarize transactions by date",
-            "8. summarize transactions by month",
-            "9. summarize transactions by year",
-            "10. summarize transactions by category",
-            "11. print this menu"]
-    
+def getJobRequest() -> int:
+    usr = input("Enter the number of a task to be completed: ").strip()
+    if usr.isdigit():
+        usr = int(usr)
+        if usr >= 0 and usr <= 11:
+            return usr
+    return -1
 
   
 if __name__ == '__main__':
-    
+    jobs = ["0. quit",
+        "1. show categories",
+        "2. add category",
+        "3. modify category",
+        "4. show transactions",
+        "5. add transaction",
+        "6. delete transaction",
+        "7. summarize transactions by date",
+        "8. summarize transactions by month",
+        "9. summarize transactions by year",
+        "10. summarize transactions by category",
+        "11. print this menu"]
+    for prompt in jobs:
+        print(prompt)
+    task = 0
+    while True:
+        task = getJobRequest()
+        if task = -1:
+            print("Invalid input, no such operation exists")
+        else:
+            
+        
+            
